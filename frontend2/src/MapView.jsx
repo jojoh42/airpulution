d40ebef7c1afa5edec7aded900f96ffac648b276
Jesper Johannsen <jjfj.johannsen@gmx.de>
2025-05-16 21:46:33 +0200

    adding an legende

--- frontend2/src/MapView.jsx
+++ frontend2/src/MapView.jsx
@@ -29,7 +29,8 @@ function MapView({ position, stations }) {
     return userLatLng.distanceTo(stationLatLng) <= searchRadius;
   };
 
-  return (
+return (
+  <>
     <MapContainer center={position} zoom={10} style={{ height: "700px", width: "100%" }} className="map-container">
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -54,30 +55,42 @@ function MapView({ position, stations }) {
         if (!coords || !isWithinRadius(coords)) return null;
 
         return (
-            <div className="map-legend">
-              <Marker
-                key={index}
-                position={[coords.latitude, coords.longitude]}
-                icon={stationIcon}
-              >
-                <Popup>
-                  <div>
-                    <strong>{station.station}</strong><br />
-                    <a href={`/station/${encodeURIComponent(station.station)}`}>PM2.5: {station.pm25[0]?.summary?.mean ?? station.pm25[0]?.value ?? "-"} µg/m³</a><br />
-                    <a href={`/station/${encodeURIComponent(station.station)}`}>PM10: {station.pm10[0]?.summary?.mean ?? station.pm25[0]?.value ?? "-"} µg/m³</a>
-                  </div>
-                </Popup>
-              </Marker>
-
-                <h4>Legende</h4>
-              <div><img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png" alt="Du" /> Deine Position</div>
-              <div><img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" alt="Station" /> Luftmessstation</div>
-              <div><span className="legend-circle"></span> Suchradius (5 km)</div>
-            </div>
+          <Marker
+            key={index}
+            position={[coords.latitude, coords.longitude]}
+            icon={stationIcon}
+          >
+            <Popup>
+              <div>
+                <strong>{station.station}</strong><br />
+                <a href={`/station/${encodeURIComponent(station.station)}`}>
+                  PM2.5: {station.pm25[0]?.summary?.mean ?? station.pm25[0]?.value ?? "-"} µg/m³
+                </a><br />
+                <a href={`/station/${encodeURIComponent(station.station)}`}>
+                  PM10: {station.pm10[0]?.summary?.mean ?? station.pm10[0]?.value ?? "-"} µg/m³
+                </a>
+              </div>
+            </Popup>
+          </Marker>
         );
       })}
     </MapContainer>
-  );
+
+    {/* Legende separat als Overlay */}
+    <div className="map-legend">
+      <h4>Legende</h4>
+      <div>
+        <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png" alt="Du" /> Deine Position
+      </div>
+      <div>
+        <img src="https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png" alt="Station" /> Luftmessstation
+      </div>
+      <div>
+        <span className="legend-circle"></span> Suchradius (20 km)
+      </div>
+    </div>
+  </>
+);
 }
 
 export default MapView;
